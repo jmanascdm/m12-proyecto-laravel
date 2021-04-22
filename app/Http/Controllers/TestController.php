97b398f457ad0b2edec40e5ef38bf1d81b6c5347
joelm <joelm@m12proyecto.dom>
2021-04-22 17:20:21 +0200

    Test view

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -2,12 +2,20 @@
 
 namespace App\Http\Controllers;
 
+use Mail;
 use Illuminate\Http\Request;
+use App\Repositories\CaptchasRepository;
 
 class TestController extends Controller
 {
     public function index()
     {
-        return view('test');
+        $captchas = new CaptchasRepository('demo', 'secret','/tmp/captchasnet-random-strings','3600','abcdefghkmnopqrstuvwxyz','6','240','80','000088');
+
+        /* Mail::send('mail.test', [], function($message) {
+            $message->to("hiimdrakster@gmail.com", "Drakster")->subject('Test de correo');
+        }); */
+
+        return view('test', compact('captchas'));
     }
 }
--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -1,12 +1,39 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
     <h1>Test Page</h1>
+    <script src="https://cdn.tiny.cloud/1/21wmjgvo3uldi678zp5poa3pc2pn0n8cu7rw8iwmp8c3r3n9/tinymce/5/tinymce.min.js"
+        referrerpolicy="origin"></script>
+  
+    <textarea>
+		Welcome to TinyMCE!
+	</textarea>
+    <script>
+        tinymce.init({
+            selector: 'textarea',
+            plugins: 'advlist autolink lists link image charmap print preview hr anchor pagebreak',
+            toolbar_mode: 'floating',
+        });
+    </script>
+
+	<h2>Captcha</h2>
+	<form method="get" action="check.php">
+		<input type="hidden" name="random" value="<?= $captchas->random () ?>" />
+		Your message:
+		<input name="message" value="Hello World" size="60" />
+		The CAPTCHA password:
+		<input name="password" size="6" />
+		<?= $captchas->image () ?> <a href="javascript:captchas_image_reload('captchas.net')">Reload Image</a>
+		<input type="submit" value="Submit" />
+	</form>
 </body>
+
 </html>

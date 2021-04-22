26c61402833d553ffa997b6e24fd09b05cabdc70
joelm <joelm@m12proyecto.dom>
2021-04-22 20:31:29 +0200

    Captcha

--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -16,5 +16,63 @@
         });
     </script>
 
+<!-- https://www.bestjquery.com/2017/07/clientsidecaptcha-pure-client-side-javascript-captcha/ -->
 	<h2>Captcha</h2>
+    <div class="captcha-chat">
+        <div class="captcha-container media">
+            <div class="media-body">
+                <p>Security Check:</p>                
+            </div>
+            <div id="captcha">
+                <div class="controls">
+                    <input class="user-text btn-common" placeholder="Type here" type="text" />
+                    <button class="btn btn-success">
+                        <!-- this image should be converted into inline svg -->
+                        <img src="img/enter_icon.png" alt="submit icon">
+                    </button>
+                    <button class="btn btn-warning">
+                        <!-- this image should be converted into inline svg -->
+                        <img src="img/refresh_icon.png" alt="refresh icon">
+                    </button>
+                </div>
+            </div>
+        </div>
+    </div>
+    </div>
+    <script src="{{ asset('js/captcha/client_captcha.js') }}" defer></script>
+    <script>
+    document.addEventListener("DOMContentLoaded", function() {
+        document.body.scrollTop; //force css repaint to ensure cssom is ready
+
+        var timeout; //global timout variable that holds reference to timer
+
+        var captcha = new $.Captcha({
+            onFailure: function() {
+
+                $(".captcha-chat .wrong").show({
+                    duration: 30,
+                    done: function() {
+                        var that = this;
+                        clearTimeout(timeout);
+                        $(this).removeClass("shake");
+                        $(this).css("animation");
+                        //Browser Reflow(repaint?): hacky way to ensure removal of css properties after removeclass
+                        $(this).addClass("shake");
+                        var time = parseFloat($(this).css("animation-duration")) * 1000;
+                        timeout = setTimeout(function() {
+                            $(that).removeClass("shake");
+                        }, time);
+                    }
+                });
+
+            },
+
+            onSuccess: function() {
+                alert("CORRECT!!!");
+            }
+        });
+
+        captcha.generate();
+    });
+    </script>
 @endsection
